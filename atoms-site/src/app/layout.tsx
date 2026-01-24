3ba14edb13641aa0819ef6d9b8b9bca7ca36d64e
Jamie Nowman <jamienowman@gmail.com>
2026-01-24 18:28:11 +0000

    Galaxy Sync: Cloudflare & Download

--- atoms-site/src/app/layout.tsx
+++ atoms-site/src/app/layout.tsx
@@ -13,6 +13,8 @@ export const metadata: Metadata = {
   description: "Automated. Atomic. Agents.",
 };
 
+export const runtime = 'edge';
+
 export default function RootLayout({
   children,
 }: Readonly<{
